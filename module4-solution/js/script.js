9f30215266dfc3ca535495da0591cc95808220b9
the-sarahb <sa.boliere@gmail.com>
2016-10-12 00:51:05 -0400

    Module 4 Solution

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -41,7 +41,16 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+for (var i=0; i<names.length; i++) {
+      var name = names[i];
+      // console.log(name)
+      // var firstLetter = function(name){
+      //   return name[0]
+      // };
+      // console.log(firstLetter(name))
+      var firstLetter = name.charAt(0).toLowerCase()
+      // console.log(firstLetter)
+      
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -57,9 +66,16 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
+  if (firstLetter ==='j') {
+    // console.log("Goodbye "+name)
+    // console.log("-0-0-0-0-0-0-0-0-0")
+    byeSpeaker.speak(name)
     // byeSpeaker.xxxx
-  } else {
+  } 
+  else {
+    // console.log("Hello "+name)
+    helloSpeaker.speak(name)
     // helloSpeaker.xxxx
   }
-}
+}//close for loop
+      // console.log(firstLetter)
